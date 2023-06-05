ed91d699c8d7b86e4f7e93c3baee984244db4499
Alan Pereyro <124737955+APereyro@users.noreply.github.com>
2023-06-05 19:22:36 -0400

    added some more file structure and pre made routes to populate pages

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,22 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
 import Auth from '../utils/auth';
 
-const Main = () => {
-    return (
-        <>
-        <div>
-            <h1>MAIN PAGE</h1>
-        </div>
-        </>
-    );
-};
 
-export default Main;
