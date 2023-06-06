9e1359ca210cc94fb876cda5b343dcc369b38f57
SurosRegime <ephrain926@hotmail.com>
2023-06-06 19:25:53 -0400

    +CSS +card
    card effect and added color and hover to button on

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,10 +1,40 @@
+<<<<<<< Updated upstream
 import React from 'react';
+=======
+import React, { useState, useEffect } from 'react';
+import './Main.css';
+import {
+  Card,
+  Spacer,
+  Button,
+  Text,
+  Input,
+  Container,
+} from '@nextui-org/react';
+>>>>>>> Stashed changes
 
 const Main = () => {
   return (
+<<<<<<< Updated upstream
     <main>
       <h2>MAIN COMING SOON</h2>
     </main>
+=======
+    <Container
+        display="flex"
+        alignItems="center"
+        justify="center"
+        css={{ minHeight: '100vh' }}
+      >
+      <Card css={{ mw: '420px', p: '20px' }}>
+    <div className="circle-container">
+      <div className="circle">
+        <p className="date">{date.toLocaleDateString()}</p>
+      </div>
+    </div>
+    </Card>
+    </Container>
+>>>>>>> Stashed changes
   );
 };
 
