59b4af343082c8863a3680da5ef324ee73f3cf56
ChemicalModel <michaelmcleod133@gmail.com>
2023-06-07 11:36:21 -0400

    fixed bugs in Main and Profile

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -16,42 +16,41 @@ const ProfilePage = () => {
     setIsOnline(!isOnline);
   };
 
- const Profile = () => { 
   return (
     <Container
-        display="flex"
-        alignItems="center"
-        justify="center"
-        css={{ minHeight: '100vh' }}
-      >
+      display="flex"
+      alignItems="center"
+      justify="center"
+      css={{ minHeight: '100vh' }}
+    >
       <Card css={{ mw: '420px', p: '20px' }}>
-    <div className="profile-page">
-      <div className="profile-picture">
-        {/* <img src="profile-picture.jpg" alt="Profile" /> */}
-      </div>
+        <div className="profile-page">
+          <div className="profile-picture">
+            {/* <img src="profile-picture.jpg" alt="Profile" /> */}
+          </div>
 
-      <div className="user-info">
-        <h2>EXAMPLE NAME</h2>
-        {isOnline ? (
-          <div className="online-indicator online">Online</div>
-        ) : (
-          <div className="online-indicator offline">Offline</div>
-        )}
-        <div className="workouts">
-          <h3>Workouts</h3>
-          <ul>
-            <li>FAV WORKOUT 1</li>
-            <li>FAV WORKOUT 2</li>
-            <li>FAV WORKOUT 3</li>
-          </ul>
+          <div className="user-info">
+            <h2>EXAMPLE NAME</h2>
+            {isOnline ? (
+              <div className="online-indicator online">Online</div>
+            ) : (
+              <div className="online-indicator offline">Offline</div>
+            )}
+            <div className="workouts">
+              <h3>Workouts</h3>
+              <ul>
+                <li>FAV WORKOUT 1</li>
+                <li>FAV WORKOUT 2</li>
+                <li>FAV WORKOUT 3</li>
+              </ul>
+            </div>
+          </div>
+
+          <button className="online-button" onClick={handleOnlineStatus}>
+            {isOnline ? 'Go Offline' : 'Go Online'}
+          </button>
         </div>
-      </div>
-
-      <button className="online-button" onClick={handleOnlineStatus}>
-        {isOnline ? 'Go Offline' : 'Go Online'}
-      </button>
-    </div>
-    </Card>
+      </Card>
     </Container>
   );
 };
