8b14ded5adfbbf34a6270c7a23b2658cfe7a2552
Alan Pereyro <124737955+APereyro@users.noreply.github.com>
2023-06-06 22:40:59 -0400

    functinal login and signup functionality and bases of main page setup

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -8,9 +8,68 @@ import {
   CardContent,
   Grid,
 } from "@mui/material";
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { ADD_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import Auth from '../../utils/auth';
+
 
 export default function SignUp() {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    heightFt: '',
+    heightIn: '',
+    weight: '',
+    age: '',
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFormState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+// console.log(formState);
+
+  // submit form
+  // submit form
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+
+  // Convert strings to numbers
+  const userData = {
+    ...formState,
+    age: parseInt(formState.age, 10),
+    weight: parseInt(formState.weight, 10),
+    heightFt: parseInt(formState.heightFt, 10),
+    heightIn: parseInt(formState.heightIn, 10)
+  };
+  
+  console.log("DATA: ", userData);
+
+    try {
+
+      const { data, error } = await addUser({
+        variables: { ...userData },
+      });
+      console.log("DATA: ", data);
+      console.log("ERROR: ", error);
+      Auth.login(data.addUser.token);
+      window.location.replace("/profile");
+    } catch (error) {
+      console.log('Erro in fetch: ', error)
+    }
+
+};
+
   return (
     <Container
       display="flex"
@@ -40,25 +99,34 @@ export default function SignUp() {
               sx={{ color: "#eec643" }}
               color="error"
               label="First Name"
+              name="firstName"
               variant="outlined"
               fullWidth
               margin="normal"
+              value={formState.firstName}
+                  onChange={handleChange}
             />
 
             <TextField
               color="error"
               label="Last Name"
+              name="lastName"
               variant="outlined"
               fullWidth
               margin="normal"
+              value={formState.lastName}
+                  onChange={handleChange}
             />
 
             <TextField
               color="error"
               label="Email"
+              name="email"
               variant="outlined"
               fullWidth
               margin="normal"
+              value={formState.email}
+                  onChange={handleChange}
               InputProps={{
                 inputMode: "email",
                 pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",
@@ -68,29 +136,38 @@ export default function SignUp() {
             <TextField
               color="error"
               label="Password"
+              name="password"
               variant="outlined"
               fullWidth
               margin="normal"
               type="password"
+              value={formState.password}
+                  onChange={handleChange}
               inputProps={{ minLength: 5 }}
             />
 
             <TextField
               color="error"
               label="Age"
+              name="age"
               variant="outlined"
               fullWidth
               margin="normal"
               type="number"
+              value={formState.age}
+                  onChange={handleChange}
               inputProps={{ maxLength: 3 }}
             />
             <TextField
               color="error"
+              name="weight"
               label="Weight (lbs)"
               variant="outlined"
               fullWidth
               margin="normal"
               type="number"
+              value={formState.weight}
+                  onChange={handleChange}
               inputProps={{ maxLength: 3 }}
             />
 
@@ -100,9 +177,12 @@ export default function SignUp() {
                   color="error"
                   label="Height (Ft)"
                   variant="outlined"
+                  name="heightFt"
                   fullWidth
                   margin="normal"
                   type="number"
+                  value={formState.heightFt}
+                  onChange={handleChange}
                   inputProps={{ maxLength: 3 }}
                 />
               </Grid>
@@ -110,15 +190,18 @@ export default function SignUp() {
                 <TextField
                   color="error"
                   label="Height (In)"
+                  name="heightIn"
                   variant="outlined"
                   fullWidth
                   margin="normal"
                   type="number"
+                  value={formState.heightIn}
+                  onChange={handleChange}
                   inputProps={{ maxLength: 3 }}
                 />
               </Grid>
             </Grid>
-
+              {/* <Link to='/main'> */}
             <Button
               variant="contained"
               type="submit"
@@ -134,9 +217,12 @@ export default function SignUp() {
                   color: "#eec643", 
                 },
               }}
+              onClick={handleFormSubmit}
             >
               Start Your Fitness Journey
             </Button>
+            {/* </Link> */}
+
           </Box>
         </CardContent>
       </Card>
