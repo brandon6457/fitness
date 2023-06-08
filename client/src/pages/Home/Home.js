125be872351265ac774ce80f746e93bbbc286f2d
SurosRegime <ephrain926@hotmail.com>
2023-06-07 22:40:35 -0400

    hide pw
    added functionality to hide password

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,5 @@
-// #eec643 saffron
-// #1c1c1c black
-// #4cb944 green
-// #fafaff white
-// #de541e orange
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import {
   Card,
   Spacer,
@@ -11,9 +7,58 @@ import {
   Text,
   Input,
   Container,
-} from '@nextui-org/react';
+} from "@nextui-org/react";
 
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 export default function Home() {
+
+  const [formState, setFormState] = React.useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value)
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    console.log('event', event)
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+
+      // redirect to home page
+      window.location.replace("/profile");
+
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       <Container
@@ -21,25 +66,27 @@ export default function Home() {
         alignItems="center"
         justify="center"
         css={{
-          minHeight: '100vh',
+          minHeight: "100vh",
         }}
       >
-        <Card css={{ mw: '420px', p: '20px' }}>
+        <Card css={{ mw: "420px", p: "20px" }}>
           <Text
             size={44}
             weight="bold"
             css={{
-              color : '#eec643',
-              textAlign: 'center', 
-              marginBottom: '20px', 
-              backgroundColor: '#4cb944',
-              borderRadius: '10px',
-              padding: '10px',
-              boxShadow: '0 0 4px #4cb944',
+              color: "#eec643",
+              textAlign: "center",
+              marginBottom: "20px",
+              backgroundColor: "#4cb944",
+              borderRadius: "10px",
+              padding: "10px",
+              boxShadow: "0 0 4px #4cb944",
             }}
           >
             Fitness Login
           </Text>
+          <form onSubmit={handleFormSubmit}>
+
           <Input
             clearable
             bordered
@@ -47,7 +94,10 @@ export default function Home() {
             color="error"
             size="lg"
             placeholder="Email"
-          />
+            name="email"
+            onChange={handleChange}
+            value={formState.email}
+            />
           <Spacer y={1} />
           <Input
             clearable
@@ -55,9 +105,16 @@ export default function Home() {
             fullWidth
             color="error"
             size="lg"
+            type={showPassword ? "text" : "password"}
+            name="password"
             placeholder="Password"
-          />
+            onChange={handleChange}
+            value={formState.password}            
+            />
           <Spacer y={1} />
+           <IconButton
+            onClick={handleShowPassword}
+            >
           <Button css={{
               color : '#4cb944',
               backgroundColor:'#eec643',
