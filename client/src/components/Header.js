4d44127284839b68aeb2e1120c1aa20768e83466
Alan Pereyro <124737955+APereyro@users.noreply.github.com>
2023-06-08 15:17:27 -0400

    fixed up header to have icon with gradient and nav tabs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
+import AuthService from '../utils/auth';
 
 const Header = () => {
-    return (
-        <div className="mainHeader">
-            <h1>Fitness</h1>
-            <h2 className="ityd">( into your day )</h2>
-        </div>
-    );
+  const logout = () => {
+    AuthService.logout();
+  };
+
+  return (
+    <div className="mainHeader">
+      <h1 className="headerTitle">
+        <FontAwesomeIcon icon={faDumbbell} className="icon" />
+        Fitness <br />
+        
+        <span className="subTitle">(into your day)</span>
+      </h1>
+
+      <ul className="headerButtons">
+        <li>
+          <Link to="/" className="headerButton">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile" className="headerButton">
+            Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="/main" className="headerButton">
+            Main
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="headerButton" onClick={logout}>
+            Log Out
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
