888fdfbf08cf005902a3d6da06ff0d5c7132e17b
Alan Pereyro <124737955+APereyro@users.noreply.github.com>
2023-06-12 22:42:09 -0400

    fixing build

--- server/server.js
+++ server/server.js
@@ -22,10 +22,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
